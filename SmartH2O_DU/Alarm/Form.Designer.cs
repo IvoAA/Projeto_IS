1b74b4c181d8eba1dc278d1695913b3555f0b9fa
Ivo Amador <ivoamador@gmail.com>
2016-12-15 12:56:48 +0000

    Refactor

---
+++ SmartH2O_DU/Alarm/Form.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Alarm
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Notifications = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Notifications";
+            // 
+            // Notifications
+            // 
+            this.Notifications.Location = new System.Drawing.Point(22, 142);
+            this.Notifications.Multiline = true;
+            this.Notifications.Name = "Notifications";
+            this.Notifications.Size = new System.Drawing.Size(240, 105);
+            this.Notifications.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(26, 83);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "All alarms active";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "PH < 3",
+            "PH = 6",
+            " 4 < NH3 < 12"});
+            this.checkedListBox1.Location = new System.Drawing.Point(22, 13);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(240, 64);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Notifications);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Notifications;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ SmartH2O_DU/Data_Uploader/DU_Program.cs
@@ -0,0 +1,50 @@
+﻿using Lib;
+using System;
+using System.Text;
+using System.Xml;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace Data_Uploader
+{
+    class DU_Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            SensorNodeDll.SensorNodeDll dll = new SensorNodeDll.SensorNodeDll();
+            dll.Initialize(NewValue, Properties.Settings.Default.Delay);
+        }
+
+        private static void NewValue(string message)
+        {
+            HandlerXML handlerXML = new HandlerXML();
+
+            XmlElement sensor = handlerXML.CreateXMLSensorFile(message);
+
+            if (handlerXML.ValidateXML(sensor.OuterXml))
+            {
+                Console.WriteLine("Channel: " + sensor.GetAttribute("element"));
+                Console.WriteLine("Data = " + sensor.OuterXml);
+                SendValue(sensor);
+            }
+
+        }
+
+        private static void SendValue(XmlElement sensor)
+        {
+            MqttClient m_cClient = new MqttClient(Properties.Settings.Default.BrokerIP);
+            string topic = sensor.GetAttribute("element");
+
+            m_cClient.Connect(Guid.NewGuid().ToString());
+
+            if (!m_cClient.IsConnected)
+            {
+                Console.WriteLine("Error connecting to message broker...");
+                return;
+            }
+            m_cClient.Publish(topic, Encoding.UTF8.GetBytes(sensor.OuterXml));
+            Console.WriteLine("Sent");
+        }
+    }
+}
